577ac11b215b67c8615f147b0b108925b13bc002
RyeSakurano <45529581+RyeSakurano@users.noreply.github.com>
2019-05-15 00:51:17 +0800

    Update Addiction.java

--- Addiction.java
+++ Addiction.java
@@ -1,4 +1,5 @@
-import java.util.*;
+package xiaoxiaole;
+
 import java.io.*;
 
 /* 防沉迷模式：
@@ -8,16 +9,50 @@ import java.io.*;
  * 【可以添加的：图形界面的倒计时】
  * 记录休息开始的时间（初始值：1970年1月1日0:00）【要用文件记录】，每局开始时比较，若超过5分钟可允许开始，并修改开始、休息时间
  */
+
+/* main函数调用：
+ * main() {
+ * 		Addiction.init();
+ * 		while (true) {
+ * 			Addiction.rest(Addiction.judgeRest());
+ * 			basePlay();
+ * 			if (Addiction.judgePlay())
+ * 				Addiction.rest(PLAY);
+ * 		}
+ * 	}
+ */
 public class Addiction {
-	private static final long REST = 300000L;
-	private static final long PLAY = 1800000L;
+	public static final long REST = 300000L;
+	public static final long PLAY = 1800000L;
 	private static long game_begin_time = -1L;
 	private static long rest_begin_time = 0L;
 	
-	/* 判断休息是否足够 */
-	public boolean judgeRest() {
+	/* 加在main函数的开始 */
+	public static void init() {
+		try {
+			File f = new File("rest.txt");
+			if (!f.exists()) {
+				DataOutputStream fout = new DataOutputStream(new FileOutputStream("rest.txt"));
+				fout.writeLong(0);
+				fout.close();
+			}
+		} catch (IOException e) {
+		}
+	}
+	
+	
+	public static void rest(long time) {
+		try {
+			Thread.sleep(time);
+		} catch (Exception e) {
+			System.exit(0);
+		}
+	}
+	
+	/* 计算休息剩余时间（毫秒） */
+	public static long judgeRest() {
 		/* 载入真实的休息开始时间，避免重启开挂 */
-		if (game_begin_time < 0L) {
+		while (game_begin_time < 0L) {
 			try {
 				DataInputStream fin = new DataInputStream(new FileInputStream("rest.txt"));
 				rest_begin_time = fin.readLong();
@@ -27,6 +62,7 @@ public class Addiction {
 		}
 		
 		long curTime = System.currentTimeMillis();
+		/* 休息完成 */
 		if (curTime - rest_begin_time > REST) {
 			game_begin_time = curTime;
 			rest_begin_time = 0;
@@ -36,15 +72,16 @@ public class Addiction {
 				fout.close();
 			} catch (IOException e) {
 			}
-			return true;
+			return 0L;
 		}
+		/* 还要休息 */
 		else {
-			return false;
+			return REST - curTime + rest_begin_time;
 		}
 	}
 	
 	/* 判断是否需要休息 */
-	public boolean judgePlay() {
+	public static boolean judgePlay() {
 		long curTime = System.currentTimeMillis();
 		if (curTime - game_begin_time > PLAY) {
 			rest_begin_time = curTime;
